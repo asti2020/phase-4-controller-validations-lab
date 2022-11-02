e447b209b982f539d27feb92b5a1f7888f566f64
Astika Luintel <astild2020@gmail.com>
2022-11-02 16:24:01 -0400

    commit tree 4c4b784ffedb01221a2cf1e92e8ac9aced1fdbf8

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,29 @@
 class PostsController < ApplicationController
+  # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+      def show
+        post = Post.find(params[:id])
+        render json: post
+      end
 
-  def show
-    post = Post.find(params[:id])
-    
-    render json: post
-  end
-
-  def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
-    render json: post
-  end
+      def update
+        post = Post.find(params[:id])
+        post.update(post_params)
+            if post.valid?
+              render json: post
+            else 
+              render json: { errors: post.errors.messages }, status: :unprocessable_entity
+            end
+      end
 
   private
 
-  def post_params
-    params.permit(:category, :content, :title)
-  end
+    def post_params
+      params.permit(:category, :content, :title)
+    end
+
+  # def render_unprocessable_entity_response(invalid)
+  #   render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  # end
 
 end
